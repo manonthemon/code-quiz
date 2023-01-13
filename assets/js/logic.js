a212c3c038f37c632a4f5896f15346ead61863f5
Manonthemon <klimowicz.maciej@gmail.com>
2023-01-13 16:10:33 +0000

    Added query selectors to some of the HTML elements and a event listener to the start quiz button

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,1 +1,8 @@
-console.log("Logic Works");
+let scores = document.querySelector("scores")
+let timer = document.querySelector("timer")
+let startButton = document.querySelector("#start")
+
+
+startButton.addEventListener("click", function() {
+console.log("button clicked")})
+
