760e9d49a66f8e6a207208bf220afb96c813e813
Manonthemon <klimowicz.maciej@gmail.com>
2023-01-13 19:40:15 +0000

    Added functions to create new question page and assign asnwers from a questionsArray to the answers buttons

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -12,55 +12,72 @@ let answersOptions = []
 
 //This is the start button with a event listener attached
 //It starts the timer
-startButton.addEventListener("click", function() {
-startTimer() //starts timer on button click
-displayQuestion()})
+//And activates the startGame function which displays first question
+startButton.addEventListener("click", function () {
+    startTimer() //starts timer on button click
+    startGame()
+})
 
 //Timer function counts down from 10
 //Resets back to 10 when finished
 var secondsLeft = 10;
 
 function startTimer() {
-  var timerInterval = setInterval(function() {
-    secondsLeft--;
-    timer.textContent = secondsLeft;
-    if(secondsLeft === 0) {
-      clearInterval(timerInterval);
-    secondsLeft = 10;
-    }
-  }, 500);
+    var timerInterval = setInterval(function () {
+        secondsLeft--;
+        timer.textContent = secondsLeft;
+        if (secondsLeft === 0) {
+            clearInterval(timerInterval);
+            secondsLeft = 10;
+        }
+    }, 1000);
 }
 
-//function displaying questions.
-// First it "disappears" the start screen
-//Then it adds text to <h2 id="question-title"></h2> 
-//Then it appends it to page
-//Then it adds questions options
+//function starting the game containing other functions controlling the game
 
-function displayQuestion(){
-     startScreen.style.display = "none";
-     questionText.textContent = "TEST QUESTION"
-     
-     document.body.appendChild(questionText);
-     newQuestionButton ()
-     newQuestionButton ()
-     newQuestionButton ()
-     newQuestionButton ()
-    };
+function startGame() {
+    newQuestion() // displays new question
+    answersButtons()
+};
 
+// function display a new questions. 
+// First it hides the start page
+//Then it creates a div for question and answers
+//then it sets the css properties of that div
+//Then it appends to the doc body
+//Then it sets the question text to the text of first question in the quizQuestions array
+//Then it appends question text to the questions div
 
-// function to add a new button (eventually containing an answer)
+let questionsContainer //variable foe the questions container defined in global scope
 
-    function newQuestionButton (){
-    let questionsContainer = document.createElement("div")
+function newQuestion() {
+    startScreen.style.display = "none";
+    questionsContainer = document.createElement("div")
     questionsContainer.style.display = "flex";
     questionsContainer.style.flexDirection = "column";
+    questionsContainer.style.textAlign = "center"
     document.body.appendChild(questionsContainer)
-    let newButton = document.createElement("button");
-    newButton.style.width = newButton.offsetWidth + "300px";
-    // Set the text content of the new button
-    newButton.textContent = "Answer 1";
-    questionsContainer.append(newButton)
-    }
+    questionText.textContent = quizQuestions[0].question1;
+    questionsContainer.appendChild(questionText);
+}
+// function to display answers buttons. 
+// It uses a for loop to make for buttons and assign it answers from the quizQuestions array
 
-    
+function answersButtons() {
+    for (let i = 0; i < 4; i++) {
+        let answerButton = document.createElement("button");
+        answerButton.textContent = quizQuestions[0].answers1[i]            
+        answerButton.style.width = answerButton.offsetWidth + "200px";
+        answerButton.style.alignSelf = "center"
+        questionsContainer.appendChild(answerButton);
+    }
+}
+
+
+
+
+
+
+
+
+
--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,1 +1,53 @@
-console.log("TEST Questions Works");
+let quizQuestions = [
+
+{ question1: "what's XXXX?",
+ answers1 : [
+    "A",
+    "B",
+    "C",
+    "D"
+],
+    correctAnswerIndex: 3
+},
+
+{ question2: "what's YYYY?",
+ answers2 : [
+    "E",
+    "F",
+    "G",
+    "H"
+],
+    correctAnswerIndex: 3
+},
+
+{ question3: "what's ZZZZ?",
+ answers3 : [
+    "I",
+    "J",
+    "K",
+    "L"
+],
+    correctAnswerIndex: 3
+},
+
+{ question4: "what's WWWW?",
+ answers4 : [
+    "M",
+    "N",
+    "O",
+    "P"
+],
+    correctAnswerIndex: 3
+},
+
+{ question5: "what's SSSS?",
+ answers5 : [
+    "Q",
+    "R",
+    "S",
+    "T"
+],
+    correctAnswerIndex: 3
+},
+
+]
